c8a71680a1bb3671664b27e036167e1f27513f3a
Ben <benschneider@dhcp-206085.eduroam.chalmers.se>
2015-07-07 19:09:45 +0200

    testing

--- SQUID_ABCD_Matrix.py
+++ SQUID_ABCD_Matrix.py
@@ -21,9 +21,9 @@ import matplotlib.pyplot as pl
 i = 1.0j
 flux0 = 2.07e-15    # Tm^2; Flux quanta: flux0 =  h / (2*charging energy)
 Z0 = 50.0           # R; Input impedance
-Z1 = 1.0            # R; Impedance of cable
+Z1 = 55.0            # R; Impedance of cable
 Z2 = 50.0           # R; Impedance of Coplanar Waveguide
-l1 = 1              # m;
+l1 = 0.1            # m;
 l2 = 200.0e-6       # m; adjust length with epsilonr for saphire *3/2
 v = 2.0e8           # m/s; approx. velocity in a coaxial 2/3 * speed of light
 Ic = 0.8e-6         # A; 0.8uA measured, 2.5 uA max
@@ -45,15 +45,15 @@ freq = dim(name = 'Frequency (GHz)',
 dim_3 = dim(name = 'Amplitude // Phase',
            start = 0,
            stop = 1,
-           pt = 2)
+           pt = 4)
 head1 = make_header(magnet, freq, dim_3, 'S11')
 Mat3d  = np.zeros((dim_3.pt,freq.pt,magnet.pt))
 
 jj = 0
 for f0 in freq.lin:
-    b = 2.0*pi*f0/v      # b = k = 2pi/wavelength; wavelength = velocity / frequency
     ii = 0
     for flux in magnet.lin:
+        b = 2.0*pi*f0/v      # b = k = 2pi/wavelength; wavelength = velocity / frequency
         L = flux0 / (Ic*2.0*pi* np.abs(cos(pi*flux/flux0)))
         Ysq = (1.0/R + 1/(i*2*pi*f0*L +i*1e-90) + i*2*pi*Cap)
         Zsq = 1.0/Ysq
@@ -61,20 +61,23 @@ for f0 in freq.lin:
         s1 = b*l1
         s2 = b*l2
         M1 = np.matrix([[cos(s1),i*Z1*sin(s1)],[i*1.0/Z1*sin(s1),cos(s1)]]) # Coaxial Cable with length l1
+        M3 = np.matrix([[0,1/Ysq],[0,1]]) # Perfectly terminated SQUID
         #M2 = np.matrix([[cos(s2),i*Z2*sin(s2)],[i*1.0/Z2*sin(s2),cos(s2)]]) # Coplanar Stripline with leght l2 (including phase modulation of the SQUID)
-        M3 = np.matrix([[1,Zsq],[0,1]]) # Imperfect terminated SQUID
-        M4 = np.matrix([[1,0],[Y4,1]]) # Wirebonds to GND
-        #M3 = np.matrix([[0,1/Ysq],[0,1]]) # Perfectly terminated SQUID
-        M = M1*M3*M4 # connect the elements
+        #M3 = np.matrix([[1,Zsq],[0,1]]) # Non Perfect termination of the SQUID
+        #M4 = np.matrix([[1,0],[Y4,1]]) # Wirebonds to GND
+        M = M1*M3 # connect the elements
         A = M[0,0]
         B = M[0,1]
         C = M[1,0]
         D = M[1,1]
         S11 = (A+B/Z0-C*Z0-D)/(A+B/Z0+C*Z0+D)
-        #S12 = 2*(A*D-B*C)/(A+B/Z0+C*Z0+D)
+        S12 = 2*(A*D-B*C)/(A+B/Z0+C*Z0+D)
 
-        Mat3d[0,jj,ii]     = 20*log(np.abs(S11))
+        Mat3d[0,jj,ii]     = np.abs(S11)
         Mat3d[1,jj,ii]     = np.angle(S11)
+        Mat3d[2,jj,ii]     = np.abs(S11)
+        Mat3d[3,jj,ii]     = np.angle(S11)
+
         ii = ii +1
     jj = jj +1
 
