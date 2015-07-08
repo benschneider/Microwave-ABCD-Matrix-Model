a53337dfcf89c3fa6c0e9e1a81fb089b1121bd1e
Ben <benschneider@dhcp-206085.eduroam.chalmers.se>
2015-07-08 14:17:58 +0200

    estimate numbers

--- SQUID_ABCD_Matrix.py
+++ SQUID_ABCD_Matrix.py
@@ -22,12 +22,12 @@ flux0 = 2.07e-15    # Tm^2; Flux quanta: flux0 =  h / (2*charging energy)
 Z0 = 50.0           # R; Input impedance
 Z1 = 20.0           # R; Impedance of cable
 Z2 = 50.0           # R; Impedance of Coplanar Waveguide
-l1 = 0.1            # m;
-l2 = 200.0e-6       # m; adjust length with epsilonr for saphire *3/2
+l1 = 0.44           # m;
+l2 = 900.0e-6*2     # m; adjust length with epsilonr for saphire
 v = 2.0e8           # m/s; approx. velocity in a coaxial 2/3 * speed of light
 Ic = 0.8e-6         # A; 0.8uA measured, 2.5 uA max
 R = 5.0e3           # Ohm
-Cap = 20.0e-15      # F
+Cap = 100.0e-15      # F
 Y4 = 1/0.001        # R; Wire bonds to GND
 
 
@@ -37,8 +37,8 @@ magnet = dim(name = 'Flux (Phi0)',
            pt = 101,
            scale = flux0)
 freq = dim(name = 'Frequency (GHz)',
-           start = 4,
-           stop = 8,
+           start = 1,
+           stop = 12,
            pt = 1001,
            scale = 1e9)
 dim_3 = dim(name = 'Amplitude // Phase',
@@ -61,15 +61,15 @@ for f0 in freq.lin:
         L = flux0 / (Ic*2.0*pi* np.abs(cos(pi*flux/flux0)))
         Ysq = (1.0/R + 1/(i*2*pi*f0*L +i*1e-90) + i*2*pi*Cap)
         Zsq = 1.0/Ysq
-        l2 = l2 + L     #Squid modulates the phase (sin(l) = l for small l)
+        #l2 = l2 + L     #Squid modulates the phase (sin(l) = l for small l) !Wrong!
         s1 = b*l1
         s2 = b*l2
         M1 = np.matrix([[cos(s1),i*Z1*sin(s1)],[i*1.0/Z1*sin(s1),cos(s1)]]) # Coaxial Cable with length l1
         #M3 = np.matrix([[0,1/Ysq],[0,1]]) # Perfectly terminated SQUID
-        #M2 = np.matrix([[cos(s2),i*Z2*sin(s2)],[i*1.0/Z2*sin(s2),cos(s2)]]) # Coplanar Stripline with leght l2 (including phase modulation of the SQUID)
+        M2 = np.matrix([[cos(s2),i*Z2*sin(s2)],[i*1.0/Z2*sin(s2),cos(s2)]]) # Coplanar Stripline with leght l2 (including phase modulation of the SQUID)
         M3 = np.matrix([[1,Zsq],[0,1]]) # Non Perfect termination of the SQUID
         M4 = np.matrix([[1,0],[Y4,1]]) # Wirebonds to GND
-        M = M1*M3*M4 # connect the elements
+        M = M1*M2*M3*M4 # connect the elements
         A = M[0,0]
         B = M[0,1]
         C = M[1,0]
