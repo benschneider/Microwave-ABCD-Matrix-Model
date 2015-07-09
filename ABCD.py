44c25e1933a5b1e7816724bdcbaf49ebbdf61013
Ben <benschneider@dhcp-206085.eduroam.chalmers.se>
2015-07-09 18:12:11 +0200

    made an ABCD matrix package

---
+++ ABCD.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Thu Jul  9 15:25:42 2015
+@author: benschneider
+'''
+
+import numpy as np
+
+#class components():
+'''
+Z = 1/Y
+n = sqrt(er*ur) #refractive index n of the material
+n ~ 3/2 #for a typical BNC cable
+b = k/n = 2*pi/(wavelength*n)
+c_light = 3e8 m/s
+wavelength = c_light/frequency
+
+2 Port circuits:
+'''
+
+def __init__(self):
+    pass
+
+def sres(Z):
+    '''
+    Series resistor on one line
+    ---|Z|---
+
+    ---------
+    M = [1 Z]
+        [0 1]
+    '''
+    M = np.matrix([[1.0,Z],[0.0,1.0]])
+    return M
+
+def terminator(Z):
+    '''
+    Termination (Impedance on line & Port 2 shorted)
+    --|Z|--|
+           |
+    -------|
+    M = [0 Z]
+        [0 1]
+    Det = 0 ! #non reciprocal network!
+    '''
+    M = np.matrix([[0.0,Z],[0.0,1.0]])
+    return M
+
+def tline(Z,b,l):
+    '''
+    Transmission line with: length l, impedance Z, and adj. wave number b:
+    ---------
+     Z, b
+    ---------
+        l       #length
+
+    M = [cos(bl)        i*Z*sin(b1)]
+        [i*Y*sin(b1)    cos(bl)]
+    '''
+    Y = 1.0/Z
+    M = np.matrix([[np.cos(b*l),1j*Z*np.sin(b*l)],[1j*Y*np.sin(b*l),np.cos(b*l)]])
+    return M
+
+def shunt(Z):
+    '''
+    Shunt resistor (1/Impedance to across both lines (i.e. to gnd))
+    ----|----
+        Y
+    ----|----
+
+    M = [1 0]
+        [Y 1]
+    '''
+    Y = 1.0/Z
+    M = np.matrix([[1.0,0.0],[Y,1.0]])
+    return M
+
+def coupling(N):
+    '''
+    # Coupling
+       N:1
+    ---} {-----
+       { }
+    ---} {-----
+
+    M = [N   0]
+        [0   1/N]
+    '''
+    M = np.matrix([[N,0.0],[0,1.0/N]])
+    return M
+
+
+class handler():
+    '''
+    Handling data recording
+    also used to convert ABCD element results
+    '''
+    def __init__(self, name = 'mag/phase' ,start = 0, stop = 7, pt = 8):
+        self.name = name     #ufo: unknown fried object
+        self.start = start
+        self.stop = stop
+        self.pt = pt
+        self.Z0 = 50
+        self.lin = np.linspace(self.start,self.stop,self.pt)
+
+    def prepare_data_save(self, dim_1, dim_2, dim_3):
+        self._SMat = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
+
+    def record_SM(self,ABCD, jj,ii):
+        '''
+        Stores the results of the S Matrix into '_SMat'
+        '''
+        S = self.get_SM(ABCD)
+        self._SMat[0,jj,ii]     = np.abs(S[0,0])
+        self._SMat[1,jj,ii]     = np.angle(S[0,0])
+        self._SMat[2,jj,ii]     = np.abs(S[0,1])
+        self._SMat[3,jj,ii]     = np.angle(S[0,1])
+        self._SMat[4,jj,ii]     = np.abs(S[1,0])
+        self._SMat[5,jj,ii]     = np.angle(S[1,0])
+        self._SMat[6,jj,ii]     = np.abs(S[1,1])
+        self._SMat[7,jj,ii]     = np.angle(S[1,1])
+
+    def unwrap_SM(self,jj):
+        #a better unwrap function is still in thinking...
+        self._SMat[1,jj]     = np.unwrap(self._SMat[1,jj])
+        self._SMat[3,jj]     = np.unwrap(self._SMat[3,jj])
+        self._SMat[5,jj]     = np.unwrap(self._SMat[5,jj])
+        self._SMat[7,jj]     = np.unwrap(self._SMat[6,jj])
+
+    def get_SM(self, M):
+        Z0 = self.Z0
+        '''
+        Returns S Matrix
+        [S11,S12]
+        [S21,S22]
+        '''
+        A = M[0,0]
+        B = M[0,1]
+        C = M[1,0]
+        D = M[1,1]
+        S11 = ( A+B/Z0-C*Z0-D ) / ( A+B/Z0+C*Z0+D )
+        S12 = 2.0*( A*D-B*C ) / ( A+B/Z0+C*Z0+D )
+        S21 = 2.0 / ( A+B/Z0+C*Z0+D )
+        S22 = ( -A+B/Z0-C*Z0+D ) / ( A+B/Z0+C*Z0+D )
+        return np.matrix([[S11,S12],[S21,S22]])
--- SQUID_ABCD_Matrix.py
+++ SQUID_ABCD_Matrix.py
@@ -12,6 +12,8 @@ For Reference on ABCD Matrix: Microwave Engineering by David M. Pozar p. 185
 import numpy as np
 from numpy import pi, cos, sin, log
 from parsers import savemtx, make_header, dim
+from ABCD import *
+
 
 import matplotlib
 matplotlib.use('macosx') # macosx, Qt4Agg, WX
@@ -20,14 +22,14 @@ import matplotlib.pyplot as pl
 i = 1.0j
 flux0 = 2.07e-15    # Tm^2; Flux quanta: flux0 =  h / (2*charging energy)
 Z0 = 50.0           # R; Input impedance
-Z1 = 30.0           # R; Impedance of 'cable'
+Z1 = 30.0           # R; Impedance of transmission piece 1
 Z2 = 50.0           # R; Impedance of Coplanar Waveguide
 l1 = 0.44           # m;
 l2 = 900.0e-6       # m; adjust length with epsilonr for saphire
 v = 2.0e8           # m/s; approx. velocity in a coaxial 2/3 * speed of light
 Ic = 1.7e-6         # A; Ic ~ 0.85uA measured, 2.5 uA max
 R = 2.3e3           # Ohm
-Cap = 450e-15       #450.0e-15     # F
+Cap = 450e-15       # 450.0e-15     # F
 Y4 = 1/0.1          # 1/Ohm; Wire bonds conductance to GND (-45dB isolation)
 
 magnet = dim(name = 'Flux (Phi0)',
@@ -40,59 +42,46 @@ freq = dim(name = 'Frequency (GHz)',
            stop = 8,
            pt = 101,
            scale = 1e9)
-dim_3 = dim(name = 'mag/phase',
+dim_3 = handler(name = 'mag/phase',
            start = 0,
-           stop = 1,
-           pt = 7)
-head1 = make_header(magnet, freq, dim_3, 'S11 S21 Z L')
-Mat3d  = np.zeros((dim_3.pt, freq.pt, magnet.pt))
+           stop = 10,
+           pt = 11) #8 pts for S 4x2 values
+dim_3._Z0 = 50
+
+head1 = make_header(magnet, freq, dim_3, 'S11 S12 S21 S22 Z L')
+dim_3.prepare_data_save(magnet, freq, dim_3)
+
 
 jj = 0
 for f0 in freq.lin:
     ii = 0
     for flux in magnet.lin:
-        b = 2.0*pi*f0/v      # b = k = 2pi/wavelength; wavelength = velocity / frequency
+        # b = k = 2pi/wavelength; wavelength = velocity / frequency
+        b = 2.0*pi*f0/v
         L = flux0 / (Ic*2.0*pi* np.abs(cos(pi*flux/flux0)))
         Ysq = (1.0/R + 1.0/(i*2.0*pi*f0*L +i*1e-90) + i*2.0*pi*f0*Cap)
         Zsq = 1.0/Ysq
-        s1 = b*l1
-        s2 = b*l2
-        #M1 = np.matrix([[cos(s1),i*Z1*sin(s1)],[i*1.0/Z1*sin(s1),cos(s1)]]) # Coaxial Cable with length l1
-        #M2 = np.matrix([[cos(s2),i*Z2*sin(s2)],[i*1.0/Z2*sin(s2),cos(s2)]]) # Coplanar Stripline with leght l2
-        M3 = np.matrix([[1.0,Zsq],[0.0,1.0]]) # SQUID Impedance Zsq(flux,freq)
-        M4 = np.matrix([[1.0,0.0],[Y4,1.0]]) # Wirebonds to GND
-        #M3 = np.matrix([[0.0,Zsq],[0.0,1.0]]) # Perfectly terminated SQUID
-        M = M3*M4 # connect the elements, simply by multiplying matrices.
-        A = M[0,0]
-        B = M[0,1]
-        C = M[1,0]
-        D = M[1,1]
-        S11 = (A+B/Z0-C*Z0-D)/(A+B/Z0+C*Z0+D)
-        S12 = 2*(A*D-B*C)/(A+B/Z0+C*Z0+D)
+
+        ABCD = tline(40,b,0.01)*tline(50,b,0.3)*tline(25,b,900e-6)*sres(Zsq)*shunt(0.1)
 
         #record stuff into matrix
-        Mat3d[0,jj,ii]     = np.abs(S11)
-        Mat3d[1,jj,ii]     = np.angle(S11)
-        Mat3d[2,jj,ii]     = np.abs(S12)
-        Mat3d[3,jj,ii]     = np.angle(S12)
-        #record what happens to L and Zsq
-        Mat3d[4,jj,ii] = np.abs(Zsq)
-        Mat3d[5,jj,ii] = np.angle(Zsq)
-        Mat3d[6,jj,ii] = np.abs(L)
+        dim_3.record_SM(ABCD,jj,ii) #dim_3._SMat is the result matrix
+        dim_3._SMat[8,jj,ii] = np.abs(Zsq) #record what happens
+        dim_3._SMat[9,jj,ii] = np.angle(Zsq)
+        dim_3._SMat[10,jj,ii] = np.abs(L)
 
         ii = ii +1
-    Mat3d[1,jj]     = np.unwrap(Mat3d[1,jj])
-    Mat3d[3,jj]     = np.unwrap(Mat3d[3,jj])
-    Mat3d[5,jj]     = np.unwrap(Mat3d[5,jj])
+    dim_3.unwrap_SM(jj)
+    dim_3._SMat[9,jj] = np.unwrap(dim_3._SMat[9,jj])
     jj = jj +1
 
 pl.figure(1)
-pl.imshow(Mat3d[0])
+pl.imshow(dim_3._SMat[0])
 pl.show()
 
 pl.figure(2)
-pl.imshow(Mat3d[1])
+pl.imshow(dim_3._SMat[1])
 pl.show()
 
-savemtx('resultdata2.mtx', Mat3d, header = head1) #mtx file can be opened by spyview
+savemtx('resultdata3.mtx', dim_3._SMat, header = head1) #mtx file can be opened by spyview
 #Link to Spyview: http://nsweb.tn.tudelft.nl/~gsteele/spyview/
