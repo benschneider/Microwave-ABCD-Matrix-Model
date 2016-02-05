6d3640647420f1f9b5e3e9414a18f20605417e46
Ben <benh.schneider@gmail.com>
2016-02-05 16:01:15 +0100

    update2()-L1-L2-L3 Buggy & M1 only 1 element

--- plotter_experimental.py
+++ plotter_experimental.py
@@ -52,8 +52,8 @@ elem.L3 = 0.01
 elem.Z4 = 0.1           # Ohm; Wire bonds conductance to GND (-45dB isolation)
 # m/s; approx. velocity in a coaxial 2/3 * speed of light
 elem.v = 2.0e8
-elem.Lbw = 1e-90
-elem.Lloop = 1e-90
+squid.Lbw = 1e-90
+squid.Lloop = 1e-90
 
 elem.update = True      # Set update to true
 
@@ -67,9 +67,10 @@ def get_sMatrix(b, elem, Zsq):
     Assembly is done for each flux point and change in Zsq
     '''
     SM = np.zeros((len(Zsq), 2, 2)) * 1j  # complex matrix
-    M1 = (tline(elem.Z1, b, elem.L1) *
-          tline(elem.Z2, b, elem.L2) *
-          tline(elem.Z3, b, elem.L3))  # transmission lines
+    # M1 = (tline(elem.Z1, b, elem.L1) *
+    #       tline(elem.Z2, b, elem.L2) *
+    #       tline(elem.Z3, b, elem.L3))  # transmission lines
+    M1 = tline(elem.Z1, b, elem.L1)
     for ii, Zsq1 in enumerate(Zsq):
         M2 = sres(Zsq1) * shunt(elem.Z4)
         M4 = M1 * M2
@@ -85,7 +86,7 @@ def get_Zsq(f0, squid):
     '''
     flux = squid.lin
     L = flux0/(squid.Ic*2.0*pi*abs(cos(pi*flux/squid.flux0)))
-    Ysq = (1.0/squid.R + 1j*2.0*pi*f0*squid.Cap - 1j/(2.0*pi*f0*L+elem.Lbw))
+    Ysq = (1.0/squid.R + 1j*2.0*pi*f0*squid.Cap - 1j/(2.0*pi*f0*L+squid.Lbw))
     return (1.0/Ysq)
 
 
@@ -117,7 +118,6 @@ def getModelData(squid, elem, measdata):
     S11 = S11*10**(measdata.ATT / 20.0)
     S11 = addphase(S11, measdata.PHI)
     elem.S11 = S11
-    print S11[0:3]
     return S11, measdata.ydat
 
 
@@ -152,7 +152,7 @@ def plotfig5(xaxis, xaxis2, S11, ydat):
     g2.plot(xaxis2, ydat.imag)
     g2.hold(False)
     plt.draw()
-    return 0
+    return
 
 
 def find_nearest(someArray, value):
@@ -166,7 +166,7 @@ def find_nearest(someArray, value):
 
 def update(val):
     if elem.update is False:
-        return 0
+        return
     f0 = sFreq.val * 1e9
     measdata.findex = find_nearest(measdata.freq, f0)
     measdata.ydat = measdata.D1complex[:, measdata.findex]
@@ -180,16 +180,16 @@ def update(val):
     elem.Z3 = np.float64(sZ3.val)
     elem.L3 = np.float64(sL3.val) * 1e-3
     elem.Z4 = np.float64(sZ4.val)
-    measdata.XSC = sXSC.val
-    measdata.XPOS = sXPOS.val
-    measdata.ATT = sATT.val
-    measdata.PHI = sPHI.val
+    measdata.XSC = np.float64(sXSC.val)
+    measdata.XPOS = np.float64(sXPOS.val)
+    measdata.ATT = np.float64(sATT.val)
+    measdata.PHI = np.float64(sPHI.val)
     S11, ydat = getModelData(squid, elem, measdata)
     xaxis = squid.xaxis
     xaxis2 = measdata.xaxis
     plotfig2(xaxis, xaxis2, S11, ydat)
     plotfig5(xaxis, xaxis2, S11, ydat)
-    return 0
+    return
 
 
 def update2(val):
@@ -203,11 +203,11 @@ def update2(val):
     sCap.set_val(squid.Cap*1e15)
     sZ1.set_val(elem.Z1)
     sZ2.set_val(elem.Z2)
-    sZ3.set_val(elem.Z3)
-    sL1.set_val(elem.L1*1e1)
-    sL2.set_val(elem.L2)
     elem.update = True
-    sL3.set_val(elem.L3*1e3)
+    sZ3.set_val(elem.Z3)
+    # sL1.set_val(elem.L1*1e1)
+    # sL2.set_val(elem.L2)
+    # sL3.set_val(elem.L3*1e3)
     T = time()-Tstart
     print "sliders are updated ", T
 
@@ -219,23 +219,22 @@ def matchXaxis(val0):
     squid.update_lin(len(x2))
     squid.matchX = True
     update(0)
-    return 0
+    return
 
 
 def preFit(val0):
-    Tstart = time()
     S11, ydat = getModelData(squid, elem, measdata)
-    zerofluxidx = find_nearest(squid.xaxis, 0.0)
+    zerofluxidx = find_nearest(measdata.xaxis, 0.0)
     t1 = np.unwrap(np.angle(ydat),  discont=pi)
     t2 = np.unwrap(np.angle(S11),  discont=pi)
     t3 = t1 - t2
-    print 'Old Phi: ', measdata.PHI
+    plt.figure(6)
+    plt.clf()
+    plt.plot(measdata.xaxis, (np.angle(S11) - np.angle(ydat)))
+    plt.draw()
     measdata.PHI = measdata.PHI + t3[zerofluxidx]
-    print 'New Phi: ', measdata.PHI
     # t2 = 20*log10(np.abs(ydat)) - 20*log10(np.abs(S11))
     # measdata.ATT = measdata.ATT - t2[zerofluxidx]
-    T = time()-Tstart
-    print "Prefit done ", T
 
 
 def addphase(Data, Phi):
@@ -269,14 +268,14 @@ def gta1(params, x, data):
     squid.Cap = params['Cap'].value
     squid.Ic = params['Ic'].value
     elem.Z1 = params['Z1'].value
-    elem.Z3 = params['Z3'].value
+    # elem.Z3 = params['Z3'].value
     preFit(False)
     return getfit() - data
 
 
 def fitcurve(val0):
     if squid.matchX is False:
-        return 0
+        return
     ydat = measdata.D1complex[:, measdata.findex]
     data = np.empty(len(ydat)*2, dtype='float64')
     data[0::2] = ydat.real
@@ -290,15 +289,17 @@ def fitcurve(val0):
     params.add('R', value=squid.R, min=1, max=1e5)
     params.add('Cap', value=squid.Cap, min=1e-15, max=1e-13)
     params.add('Z1', value=squid.Cap, min=25, max=100)
-    params.add('Z3', value=squid.Cap, min=25, max=100)
+    params.add('Lbw', value=squid.Lbw, min=1e-90, max=1e-10)
+    # params.add('Z3', value=squid.Cap, min=25, max=100)
     result = minimize(gta1, params, args=(xaxis3, data))
     print report_fit(result)
 
     squid.Ic = result.params['Ic'].value
     squid.R = result.params['R'].value
     squid.Cap = result.params['Cap'].value
+    squid.Lbw = result.params['Lbw'].value
     elem.Z1 = result.params['Z1'].value
-    elem.Z3 = result.params['Z3'].value
+    # elem.Z3 = result.params['Z3'].value
     update2(0)
     preFit(True)
 
@@ -310,12 +311,12 @@ def fitcurve(val0):
     plt.plot(xaxis3, residual)
     plt.draw()
     print abs(np.mean((residual*residual)))*1e8
-    return 0
+    return
 
 
 def xyFind(val):
     preFit(0)
-    return 0
+    return
 
 
 # --- Interface Buttons and Sliders ---
