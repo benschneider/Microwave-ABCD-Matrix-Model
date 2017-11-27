7e19fc259c9597ce3185c4c23af1226aa417b39d
Ben <benh.schneider@gmail.com>
2017-11-27 14:33:06 +0100

    Added 3-Port Matrix

--- ABCD.py
+++ ABCD.py
@@ -6,7 +6,7 @@ Created on Thu Jul  9 15:25:42 2015
 
 import numpy as np
 
-#class components():
+# class components():
 '''
 Z = 1/Y
 n = sqrt(er*ur) #refractive index n of the material
@@ -19,10 +19,12 @@ wavelength = c_light/frequency
 Ref: 'Microwave Engineering 3rd Edition' by David M. Pozar p. 185
 '''
 
+
 def __init__(self):
     pass
 
 #__all__ = ["sres", "terminator", "tline", "shunt", "coupling", "handler"]
+
 
 def sres(Z):
     '''
@@ -33,8 +35,9 @@ def sres(Z):
     M = [1 Z]
         [0 1]
     '''
-    M = np.matrix([[1.0,Z],[0.0,1.0]])
+    M = np.matrix([[1.0, Z], [0.0, 1.0]])
     return M
+
 
 def terminator(Z):
     '''
@@ -46,10 +49,11 @@ def terminator(Z):
         [0 1]
     Det = 0 ! #non reciprocal network!
     '''
-    M = np.matrix([[0.0,Z],[0.0,1.0]])
+    M = np.matrix([[0.0, Z], [0.0, 1.0]])
     return M
 
-def tline(Z,b,l):
+
+def tline(Z, b, l):
     '''
     Transmission line with: length l, impedance Z, and adj. wave number b:
     ---------
@@ -62,9 +66,11 @@ def tline(Z,b,l):
     '''
     if Z == 0:
         Z = 1e-100
-    Y = 1.0/Z
-    M = np.matrix([[np.cos(b*l),1j*Z*np.sin(b*l)],[1j*Y*np.sin(b*l),np.cos(b*l)]])
+    Y = 1.0 / Z
+    M = np.matrix([[np.cos(b * l), 1j * Z * np.sin(b * l)],
+                   [1j * Y * np.sin(b * l), np.cos(b * l)]])
     return M
+
 
 def shunt(Z):
     '''
@@ -77,11 +83,12 @@ def shunt(Z):
         [Y 1]
     '''
     if Z == 0:
-        Z = 1e-100
+        Z = 1e-100  # avoid 1/0 division
 
-    Y = 1.0/Z
-    M = np.matrix([[1.0,0.0],[Y,1.0]])
+    Y = 1.0 / Z
+    M = np.matrix([[1.0, 0.0], [Y, 1.0]])
     return M
+
 
 def coupling(N):
     '''
@@ -94,7 +101,21 @@ def coupling(N):
     M = [N   0]
         [0   1/N]
     '''
-    M = np.matrix([[N,0.0],[0,1.0/N]])
+    M = np.matrix([[N, 0.0], [0, 1.0 / N]])
+    return M
+
+
+def threeport(Z1, Z2, Z3):
+    '''
+    -Z1-|-Z2-
+        Z3
+    ----|----
+    '''
+    A = 1.0 + Z1 / Z3
+    B = Z1 + Z2 + Z1 * Z2 / Z3
+    C = 1.0 / Z3
+    D = 1.0 + Z2 / Z3
+    M = np.matrix([A, B], [C, D])
     return M
 
 
@@ -103,49 +124,50 @@ class handler():
     Handling data recording
     also used to convert ABCD element results
     '''
-    def __init__(self, name = 'mag/phase' ,start = 0, stop = 7, pt = 8):
-        self.name = name     #ufo: unknown fried object
+
+    def __init__(self, name='mag/phase', start=0, stop=7, pt=8):
+        self.name = name  # ufo: unknown fried object
         self.start = start
         self.stop = stop
         self.pt = int(pt)
         self.Z0 = 50
-        self.lin = np.linspace(self.start,self.stop,self.pt)
+        self.lin = np.linspace(self.start, self.stop, self.pt)
 
-    def update_lin(self,pt):
+    def update_lin(self, pt):
         self.pt = int(pt)
-        self.lin = np.linspace(self.start,self.stop,self.pt)
+        self.lin = np.linspace(self.start, self.stop, self.pt)
 
     def prepare_data_save(self, dim_1, dim_2, dim_3):
         self._SMat = np.zeros((dim_3.pt, dim_2.pt, dim_1.pt))
 
-    def record_SM(self,ABCD, jj,ii):
+    def record_SM(self, ABCD, jj, ii):
         '''
         Stores the results of the S Matrix into '_SMat'
         '''
         S = self.get_SM(ABCD)
-        self._SMat[0,jj,ii]     = np.abs(S[0,0])
-        self._SMat[1,jj,ii]     = np.angle(S[0,0])
-        self._SMat[2,jj,ii]     = np.abs(S[0,1])
-        self._SMat[3,jj,ii]     = np.angle(S[0,1])
-        self._SMat[4,jj,ii]     = np.abs(S[1,0])
-        self._SMat[5,jj,ii]     = np.angle(S[1,0])
-        self._SMat[6,jj,ii]     = np.abs(S[1,1])
-        self._SMat[7,jj,ii]     = np.angle(S[1,1])
+        self._SMat[0, jj, ii] = np.abs(S[0, 0])
+        self._SMat[1, jj, ii] = np.angle(S[0, 0])
+        self._SMat[2, jj, ii] = np.abs(S[0, 1])
+        self._SMat[3, jj, ii] = np.angle(S[0, 1])
+        self._SMat[4, jj, ii] = np.abs(S[1, 0])
+        self._SMat[5, jj, ii] = np.angle(S[1, 0])
+        self._SMat[6, jj, ii] = np.abs(S[1, 1])
+        self._SMat[7, jj, ii] = np.angle(S[1, 1])
 
-    def record_ZL(self,Z,L, jj,ii):
+    def record_ZL(self, Z, L, jj, ii):
         '''
         Stores the results of the S Matrix into '_SMat'
         '''
-        self._SMat[8,jj,ii] = np.abs(Z)
-        self._SMat[9,jj,ii] = np.angle(Z)
-        self._SMat[10,jj,ii] = np.abs(L)
+        self._SMat[8, jj, ii] = np.abs(Z)
+        self._SMat[9, jj, ii] = np.angle(Z)
+        self._SMat[10, jj, ii] = np.abs(L)
 
-    def unwrap_SM(self,jj):
-        #a better unwrap function is still in thinking...
-        self._SMat[1,jj]     = np.unwrap(self._SMat[1,jj])
-        self._SMat[3,jj]     = np.unwrap(self._SMat[3,jj])
-        self._SMat[5,jj]     = np.unwrap(self._SMat[5,jj])
-        self._SMat[7,jj]     = np.unwrap(self._SMat[6,jj])
+    def unwrap_SM(self, jj):
+        # a better unwrap function is still in thinking...
+        self._SMat[1, jj] = np.unwrap(self._SMat[1, jj])
+        self._SMat[3, jj] = np.unwrap(self._SMat[3, jj])
+        self._SMat[5, jj] = np.unwrap(self._SMat[5, jj])
+        self._SMat[7, jj] = np.unwrap(self._SMat[6, jj])
 
     def get_SM(self, M):
         Z0 = self.Z0
@@ -154,12 +176,12 @@ class handler():
         [S11,S12]
         [S21,S22]
         '''
-        A = M[0,0]
-        B = M[0,1]
-        C = M[1,0]
-        D = M[1,1]
-        S11 = ( A+B/Z0-C*Z0-D ) / ( A+B/Z0+C*Z0+D )
-        S12 = 2.0*( A*D-B*C ) / ( A+B/Z0+C*Z0+D )
-        S21 = 2.0 / ( A+B/Z0+C*Z0+D )
-        S22 = ( -A+B/Z0-C*Z0+D ) / ( A+B/Z0+C*Z0+D )
-        return np.matrix([[S11,S12],[S21,S22]])
+        A = M[0, 0]
+        B = M[0, 1]
+        C = M[1, 0]
+        D = M[1, 1]
+        S11 = (A + B / Z0 - C * Z0 - D) / (A + B / Z0 + C * Z0 + D)
+        S12 = 2.0 * (A * D - B * C) / (A + B / Z0 + C * Z0 + D)
+        S21 = 2.0 / (A + B / Z0 + C * Z0 + D)
+        S22 = (-A + B / Z0 - C * Z0 + D) / (A + B / Z0 + C * Z0 + D)
+        return np.matrix([[S11, S12], [S21, S22]])
